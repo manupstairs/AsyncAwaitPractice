fc734233543c2aa276711f755c4a7a999f73f714
xingzheng <xingzheng@outlook.com>
2020-01-10 17:20:58 +0800

    Add project files.

---
+++ AsyncExceptionHandle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AsyncExceptionHandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tester = new CatchAsyncExceptionTester();
+            try
+            {
+                tester.AsyncVoidExceptions_CannotBeCaughtByCatch();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The topmost try catch");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WpfAppWithException/AsyncExceptionTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppWithException
+{
+    class AsyncExceptionTester
+    {
+        private async void ThrowExceptionAsync()
+        {
+            await Task.Delay(100);
+            throw new InvalidOperationException();
+        }
+
+        public void AsyncVoidExceptions_CannotBeCaughtByCatch()
+        {
+            try
+            {
+                ThrowExceptionAsync();
+            }
+            catch (Exception)
+            {
+                // The exception is never caught here!
+                throw;
+            }
+        }
+    }
+}
